b55d9b0bb9f288a0794303a573e3b17f38b06423
VineethK <vineethk96@users.noreply.github.com>
2025-05-03 20:02:21 +0100

    chore: added another album

--- vinyl-wall/src/App.tsx
+++ vinyl-wall/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
   const [records, setRecords] = useState<RecordData[]>([
     { id: 1, imageUrl: '/images/Traveler_Album.png' },
     { id: 2, imageUrl: '/images/HotStone_Album.png' },
-    { id: 3, imageUrl: '/images/record3.jpg' },
+    { id: 3, imageUrl: '/images/gestureRecog_Album.png' },
     { id: 4, imageUrl: '/images/record4.jpg' },
     { id: 5, imageUrl: '/images/record5.jpg' },
     { id: 6, imageUrl: '/images/record6.jpg' },
