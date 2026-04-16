81b7f0d61ed7f73409bf76ecdc02df0fbc5ca3d5
vineethk96 <vineethk96@users.noreply.github.com>
2026-04-16 09:26:15 +0000

    Deploying to gh-pages from @ vineethk96/vineethk96.github.io@02b1f5011ad4bbc1b44fdde2ef569508907f5977 🚀

--- assets/CADModelViewer-DuKlZjQb.js
+++ assets/CADModelViewer-DuKlZjQb.js
@@ -1,4 +1,4 @@
-import{r as Ge,g as Wy,j as xt}from"./index-BV2wuhM5.js";/**
+import{r as Ge,g as Wy,j as xt}from"./index-DGfg7u55.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
